08f32c3000cb750b53620c9a6ab05086ba0a743e
HoSHIZA <hoshiza78@gmail.com>
2024-08-22 00:37:26 +0300

    Added EnumExtensions for EnumFlags

---
+++ Code/Runtime/Extensions/EnumExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NiGames.Essentials
+{
+    public static class EnumExtensions
+    {
+        public static T SetFlag<T>(this T value, T flag) where T : struct, Enum
+        {
+            return (T)(object)((int)(object)value | (int)(object)flag);
+        }
+        
+        public static T ClearFlag<T>(this T value, T flag) where T : struct, Enum
+        {
+            return (T)(object)((int)(object)value & ~(int)(object)flag);
+        }
+        
+        public static T ToggleFlag<T>(this T value, T flag) where T : struct, Enum
+        {
+            return (T)(object)((int)(object)value ^ (int)(object)flag);
+        }
+        
+        public static bool HasFlag<T>(this T value, T flag) where T : struct, Enum
+        {
+            return ((int)(object)value & (int)(object)flag) != 0;
+        }
+    }
+}
