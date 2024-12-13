9a2011ae637c4b8f7bcde106320e1967bf1890f1
HoSHIZA <hoshiza78@gmail.com>
2024-12-13 14:47:45 +0300

    Anchoring algorithm has been optimised.

--- Code/Runtime/Components/UI/AnchorSnap.cs
+++ Code/Runtime/Components/UI/AnchorSnap.cs
@@ -1,115 +1,61 @@
-﻿using UnityEngine;
+﻿using JetBrains.Annotations;
+using UnityEngine;
 
 namespace NiGames.Essentials.Components
 {
     /// <summary>
-    /// A component that provides functionality to snap a RectTransform's anchor to a predefined grid.
+    /// A component that provides functionality to snap a RectTransform's anchor automatically.
     /// </summary>
-    /// <remarks>
-    /// This component creates a predefined grid of segments based on a specified number of rows and columns.
-    /// The anchor of the RectTransform can then be snapped to one of the grid segments using the <see cref="Snap"/> method.
-    /// </remarks>
     [DisallowMultipleComponent]
     [RequireComponent(typeof(RectTransform))]
     [AddComponentMenu(Constants.Menu.Component.UI + "Anchor Snap")]
     public sealed class AnchorSnap : MonoBehaviour
     {
-        public readonly struct Segment
-        {
-            public readonly Rect Rect;
-            public readonly Vector2 Anchor;
+        [Tooltip("Container in which the alignment will take place.")] [SerializeField]
+        private RectTransform _container;
 
-            public Segment(Rect rect, Vector2 anchor)
-            {
-                Rect = rect;
-                Anchor = anchor;
-            }
-        }
-        
-        public const int SEGMENT_COUNT = 3;
-        
-        [Tooltip("Container in which the alignment will take place.")]
-        [SerializeField] private RectTransform _container;
-        
-        private Segment[] _segments;
-        
-        private RectTransform _rectTransform;
-        private RectTransform RectTransform => _rectTransform ? _rectTransform : _rectTransform = (RectTransform)transform;
-        
-        private void Start()
+        private RectTransform _rt;
+
+        private void Awake()
         {
-            _container = _container ? _container : (RectTransform)transform.parent;
-            
-            Calculate();
+            _rt = (RectTransform)transform;
+            _container ??= (RectTransform)transform.parent;
         }
-        
-        private void OnRectTransformDimensionsChange()
-        {
-            Calculate();
-        }
-        
+
         /// <summary>
-        /// Calculates the grid of segments based on the number of segments and container RectTransform.
+        /// Automatically calculates the anchor snap based on the current local position within the container.
         /// </summary>
-        public void Calculate()
-        {
-            _container.pivot = Vector2.zero;
-
-            var totalSegments = SEGMENT_COUNT * SEGMENT_COUNT;
-            var segmentWidth = _container.rect.width / SEGMENT_COUNT;
-            var segmentHeight = _container.rect.height / SEGMENT_COUNT;
-            
-            if (_segments.Length != totalSegments)
-            {
-                _segments = new Segment[totalSegments];
-            }
-            
-            for (var i = 0; i < totalSegments; i++)
-            {
-                var column = i % SEGMENT_COUNT;
-                var row = i / SEGMENT_COUNT;
-                
-                var rect = new Rect(segmentWidth * column, segmentHeight * row, segmentWidth, segmentHeight);
-
-                var anchor = new Vector2
-                {
-                    x = column switch
-                    {
-                        0 => 0f,
-                        SEGMENT_COUNT - 1 => 1f,
-                        _ => 0.5f
-                    },
-                    y = row switch
-                    {
-                        0 => 0f,
-                        SEGMENT_COUNT - 1 => 1f,
-                        _ => 0.5f
-                    }
-                };
-
-                _segments[i] = new Segment(rect, anchor);
-            }
-        }
-        
-        /// <summary>
-        /// Snaps the anchor of the RectTransform to the closest segment in the grid.
-        /// </summary>
+        [PublicAPI]
         public void Snap()
         {
-            var localPosition = RectTransform.localPosition;
-            
-            foreach (var segment in _segments)
-            {
-                var rect = segment.Rect;
-                
-                if (!rect.Contains(localPosition)) continue;
-                
-                RectTransform.anchorMin = segment.Anchor;
-                RectTransform.anchorMax = segment.Anchor;
-                RectTransform.localPosition = localPosition;
-                    
-                break;
-            }
+            var absolutePosition = CalculateAbsolutePosition(_rt);
+            var normalizedX = absolutePosition.x / _container.rect.size.x + _container.pivot.x;
+            var normalizedY = absolutePosition.y / _container.rect.size.y + _container.pivot.y;
+
+            var anchorX = CalculateAnchorValue(normalizedX);
+            var anchorY = CalculateAnchorValue(normalizedY);
+            var anchor = new Vector2(anchorX, anchorY);
+
+            var worldPosition = _rt.position;
+
+            _rt.anchorMin = anchor;
+            _rt.anchorMax = anchor;
+
+            _rt.position = worldPosition;
         }
+        
+        private static Vector2 CalculateAbsolutePosition(RectTransform rt)
+        {
+            var pivotOffset = new Vector2(rt.rect.width * (rt.pivot.x - 0.5f), rt.rect.height * (rt.pivot.y - 0.5f));
+
+            return (Vector2)rt.localPosition - pivotOffset;
+        }
+        
+        private static float CalculateAnchorValue(float v) => v switch
+        {
+            < 0.33f => 0f,
+            >= 0.66f => 1f,
+            _ => 0.5f,
+        };
     }
 }
